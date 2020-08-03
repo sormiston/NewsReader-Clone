c4ca5654fbc1f6cde2f35b4360b1db4a56645e82
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-03 19:53:43 -0400

    must buffer data load.

--- newsreader-clone/src/App.js
+++ newsreader-clone/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 import MainFeed from './MainFeed.js'
 
 function App() {
+  // Fake user Login - friend of mine
+  localStorage.setItem('name', 'Guadalupe')
+  localStorage.setItem('profile', 'https://www.facebook.com/guadalupe.peraza.925?__tn__=%3Chttps://scontent-lga3-1.xx.fbcdn.net/v/t1.0-1/p480x480/101024330_10164188155135599_1823391583416877056_n.jpg?_nc_cat=103&_nc_sid=7206a8&_nc_ohc=0QXAxUwnlQoAX-NKA6A&_nc_oc=AQn2agWQh1hAf-9kR1i_53AeD2lPFaS3z5QOgCX6wzGaO-zdrId2HtrN1q8_o0gTd9Q&_nc_ht=scontent-lga3-1.xx&_nc_tp=6&oh=780f7d95e5f2bb05e1037e0a7ded7d4f&oe=5F4EF738')
+
   return (
     <div className="App">
       <nav>
-        <Link to="/" exact>Mydium</Link> 
+        <Link to="/" exact>Mydium</Link><span> searchIcon bookmarkIcon Notifications userProfile</span>
       </nav>
       <Route path="/" exact>
         <MainFeed />
--- newsreader-clone/src/MainFeed.js
+++ newsreader-clone/src/MainFeed.js
@@ -14,15 +14,43 @@ export default function MainFeed() {
           Authorization: `Bearer ${REACT_APP_AIRTABLE_API_KEY}`
         }
       })
-      setArticles(res.data.records)
+      console.log(res.data.records)
+      
+      function randomizeArticles (array) {
+        const randomized = []
+        while (array.length > 0) {
+          const randomIndex = Math.floor(Math.random() * array.length)
+          randomized.push(...array.splice(randomIndex, 1)) // still not 100% HOW spread operator works, but thankful that it breaks unecessary nesting
+        }
+        return randomized
+      }
+      setArticles(randomizeArticles(res.data.records))
+
     }
     apiCall()
   }, [])
-  console.log(Object.keys(process.env))
-  console.log(articles)
+
+  function guard() {
+    return articles.length > 0 ? true : 'SPINNING THINGEE'
+  }
   
-  return <>
-    Yes Yes YA DONT STOP
-     </>
-     
-    }
+  console.log(articles.length > 0)
+
+  return <main>
+    <div className="container">Category Carousel</div>
+    <div>Tiles and Wrappers to carve out the real estate</div>
+    <Link to='/:id'>
+      <div className="box">
+        <article className="media">
+          <div className="media-content">
+            <div className="content is-large">{guard() && articles[0].headline}</div>
+          </div>
+        </article>
+      </div>
+    </Link>
+   
+
+
+  </main>
+
+}
